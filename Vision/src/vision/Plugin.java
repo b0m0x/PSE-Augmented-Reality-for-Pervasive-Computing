cca592da2891dbdcbaab093b63ad408511c3d112
Shinobi18 <thomaskthorn@googlemail.com>
2011-12-08 01:43:19 +0100

    added id for testing

--- Vision/src/vision/Plugin.java
+++ Vision/src/vision/Plugin.java
@@ -3,5 +3,44 @@ package vision;
 import java.util.List;
 
 public abstract class Plugin {
+	/** 
+	 * @uml.property name="tags"
+	 */
 	private List<String> tags;
+	/**
+	 * @uml.property  name="id"
+	 */
+	private int id;
+
+	/**
+	 * Getter of the property <tt>id</tt>
+	 * @return  Returns the id.
+	 * @uml.property  name="id"
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Setter of the property <tt>id</tt>
+	 * @param id  The id to set.
+	 * @uml.property  name="id"
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/** 
+	 * @uml.property  name="tags"
+	 */
+	public List<String> getTags() {
+		return tags;
+	}
+
+	/** 
+	 * @uml.property  name="tags"
+	 */
+	public void setTags(String tags) {
+		this.tags.add(tags);
+	}
 }
