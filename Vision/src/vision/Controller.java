b681fcbc93538ac42800802167d467718a82ca5f
Shinobi18 <thomaskthorn@googlemail.com>
2011-12-09 23:28:40 +0100

    added Controller

---
+++ Vision/src/vision/Controller.java
@@ -0,0 +1,28 @@
+package vision;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+
+public class Controller implements ScreenController {
+
+	@Override
+	public void bind(Nifty arg0, Screen arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEndScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStartScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
