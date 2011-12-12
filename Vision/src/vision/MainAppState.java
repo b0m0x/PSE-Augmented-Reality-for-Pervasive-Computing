34a3294e067b50658b0d6bbcab274043f26b53d1
Shinobi18 <thomaskthorn@googlemail.com>
2011-12-12 23:10:46 +0100

    added MainAppState and GuiAppState

---
+++ Vision/src/vision/MainAppState.java
@@ -0,0 +1,14 @@
+package vision;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+
+
+public class MainAppState extends AbstractAppState {
+	@Override
+	public void initialize(AppStateManager stateManager, Application app) {
+		// TODO Auto-generated method stub
+		super.initialize(stateManager, app);
+	}
+}
