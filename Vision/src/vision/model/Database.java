6e15446c77647a5b408ca8500554f87e67874f4e
Marek Wehmer <marek.wehmer@googlemail.com>
2011-12-16 12:02:18 +0100

    added database function for fetching sensordata of a defined interval

--- Vision/src/vision/model/Database.java
+++ Vision/src/vision/model/Database.java
@@ -24,11 +24,22 @@ public class Database {
 	}
 	
 	/**
-	 * 
+	 * returns all stored samples of a sensor
 	 * @param id id of the sensor
 	 * @return a list of all sensor samples belonging to the given sensor
 	 */
 	public List<Sample> getAllSensorData(int id) {
+		throw new UnsupportedOperationException();	
+	}
+	
+	/**
+	 * returns all samples of a sensor inbetween a given interval
+	 * @param id id of the sensor
+	 * @param from timestamp of beginning 
+	 * @param to timestamp of the end of the interval
+	 * @return a list of all sensor samples belonging to the given sensor
+	 */
+	public List<Sample> getSensorDataInterval(int id, long from, long to) {
 		throw new UnsupportedOperationException();	
 	}
 
