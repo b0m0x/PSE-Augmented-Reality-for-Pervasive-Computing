dd68bda289c50929acc49f9466971b6d1fac4382
Marek Wehmer <marek.wehmer@googlemail.com>
2011-12-19 00:57:55 +0100

    fixed javadoc creation script (src path issue) updated javadoc

--- create_javadoc.sh
+++ create_javadoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javadoc -sourcepath src -subpackages -package vision -package vision.model -subpackages vision.controller vision.view -doclet com.aurigalogic.doclet.core.Doclet -docletpath aurigadoclet/bin/AurigaDoclet.jar -format pdf -out doc.pdf
+javadoc -sourcepath Vision/src -subpackages -package vision -package vision.model -subpackages vision.controller vision.view -doclet com.aurigalogic.doclet.core.Doclet -docletpath aurigadoclet/bin/AurigaDoclet.jar -format pdf -out doc.pdf
 
