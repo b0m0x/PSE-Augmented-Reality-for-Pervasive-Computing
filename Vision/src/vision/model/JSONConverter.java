0c00ef10df2d94d9845e8568240c75067ddce2cb
Enes13 <enese31@hotmail.de>
2011-12-22 17:20:48 +0100

    added a few methods

--- Vision/src/vision/model/JSONConverter.java
+++ Vision/src/vision/model/JSONConverter.java
@@ -1,30 +1,74 @@
 package vision.model;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
+import org.json.HTTP;
+import org.json.JSONException;
 import org.json.JSONObject;
-
 
 public class JSONConverter {
 
 	private JSONObject json;
 	private Sensor sensor;
 	private List<Sensor> sensorList;
-	
-	public void getUrl() {
-		
+
+	public String getUrl() {
+		return "";
 	}
-	
+
 	public void convert() {
-		
+		try {
+			json = HTTP.toJSONObject(getUrl());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		sensor = new Sensor();
+		try {
+			
+			sensor.setDescription(json.getString("Declassscription"));
+			sensor.setDescription(json.getString("Description"));
+			sensor.setId(json.getString("ID"));
+			sensor.setMesswert(new Sample()); //create sample method
+			sensor.setPosition(new Position()); //create position method
+			sensor.setRegistered(true); //create registered method
+			sensor.setTags(null); //create tags method
+			sensor.setUpdate(Integer.parseInt(json.getString("Update")));
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			this.addSensorToList();
+		}
+
 	}
 	
+	private Sample makeSample(String s) {
+		return null;
+	}
+	
+	private Position makePosition(String s) {
+		return null;
+	}
+	
+	private boolean makeRegistered(String s) {
+		return false;
+	}
+	
+	private List<String> makeTags(String s) {
+		return null;
+	}
+
 	public void addSensorToList() {
-		
+		this.sensorList.add(this.sensor);
 	}
-	
+
 	public List<Sensor> getSensorList() {
 		return this.sensorList;
 	}
-	
+
 }
--- Vision/src/vision/model/Update.java
+++ Vision/src/vision/model/Update.java
@@ -1,22 +1,32 @@
 package vision.model;
 
+import java.util.List;
+import java.sql.*;
+
 /**
  * manages the server connection and fetches the sensor data
- *
+ * 
  */
 public class Update {
 
+	private Connection conn;
 
 	/**
-	 * @uml.property   name="daten"
-	 * @uml.associationEnd   inverse="update:vision.model.Model"
+	 * @uml.property name="daten"
+	 * @uml.associationEnd inverse="update:vision.model.Model"
 	 */
+	/**
+	 * provides a facade for all objects belonging to the model
+	 * 
+	 */
+
 	private Model daten;
 
 	/**
 	 * Getter of the property <tt>daten</tt>
-	 * @return  Returns the daten.
-	 * @uml.property  name="daten"
+	 * 
+	 * @return Returns the daten.arg0
+	 * @uml.property name="daten"
 	 */
 	public Model getDaten() {
 		return daten;
@@ -24,70 +34,108 @@ public class Update {
 
 	/**
 	 * Setter of the property <tt>daten</tt>
-	 * @param daten  The daten to set.
-	 * @uml.property  name="daten"
+	 * 
+	 * @param daten
+	 *            The daten to set.
+	 * @uml.property name="daten"
 	 */
 	public void setDaten(Model daten) {
 		this.daten = daten;
 	}
 
-	/** 
+	/**
 	 * @uml.property name="jSONConverter"
 	 * @uml.associationEnd inverse="update:vision.model.JSONConverter"
 	 */
 	private JSONConverter jsonConverter;
 
-	/** 
+	/**
 	 * Getter of the property <tt>jSONConverter</tt>
-	 * @return  Returns the jsonConverter.
-	 * @uml.property  name="jSONConverter"
+	 * 
+	 * @return Returns the jsonConverter.
+	 * @uml.property name="jSONConverter"
 	 */
 	public JSONConverter getJSONConverter() {
 		return jsonConverter;
 	}
 
-	/** 
-	 * Setter of the property <tt>jSONConverter</tt>
-	 * @param jSONConverter  The jsonConverter to set.
-	 * @uml.property  name="jSONConverter"
+	/**
+	 * Setter of the property <tt>jSONConverter</tt> arg0
+	 * 
+	 * @param jSONConverter
+	 *            The jsonConverter to set.
+	 * @uml.property name="jSONConverter"
 	 */
 	public void setJSONConverter(JSONConverter jsonConverter) {
 		this.jsonConverter = jsonConverter;
 	}
 
 	/**
-	 * @uml.property  name="database"
-	 * @uml.associationEnd  inverse="update:vision.model.Database"
+	 * @uml.property name="database"
+	 * @uml.associationEnd inverse="update:vision.model.Database"
 	 */
 	private Database database;
 
 	/**
 	 * Getter of the property <tt>database</tt>
-	 * @return  Returns the database.
-	 * @uml.property  name="database"
+	 * 
+	 * @return Returns the database.
+	 * @uml.property name="database"
 	 */
 	public Database getDatabase() {
 		return database;
 	}
 
+	private void connect() {
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection("jdbc:h2:", "", "");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void disconnect() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Setter of the property <tt>database</tt>
-	 * @param database  The database to set.
-	 * @uml.property  name="database"
+	 * 
+	 * @param database
+	 *            The database to set.
+	 * @uml.property name="database"
 	 */
 	public void setDatabase(Database database) {
 		this.database = database;
 	}
-	
-		
-		
-		public void store(int time){
-		}
 
-		
-		/**
-		 */
-		public void getAllData(){
+	public void store(int time) {
+		List<Sensor> sensorList = jsonConverter.getSensorList();
+		this.connect();
+		try {
+			Statement statement = conn.createStatement();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+
+	}
+
+	/**
+		 */
+	public void getAllData() {
+	}
 
 }
