453118569697ea0ca0d91ad291f00469e17732ad
Shinobi18 <thomaskthorn@googlemail.com>
2012-01-18 17:21:47 +0100

    windowplugin class edited not finished yet

--- Vision/src/vision/view/WindowPlugin.java
+++ Vision/src/vision/view/WindowPlugin.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import vision.controller.WindowController;
 import vision.model.Model;
+import vision.model.Sample;
+import vision.model.Sensor;
 
 import com.jme3.app.Application;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.material.Material;
 import com.jme3.scene.Geometry;
 
 public class WindowPlugin extends Plugin {
@@ -14,43 +18,70 @@ public class WindowPlugin extends Plugin {
 	 * @uml.property name="windows"
 	 */
 	private List<Geometry> windows;
+	private Geometry window;
 
 	/**
-	 * Getter of the property <tt>windows</tt>
-	 * 
-	 * @return Returns the windows.
-	 * @uml.property name="windows"
 	 */
-	public List<Geometry> getWindows() {
-		return windows;
-	}
-
-	/**
-	 * Setter of the property <tt>windows</tt>
-	 * 
-	 * @param windows
-	 *            The windows to set.
-	 * @uml.property name="windows"
-	 */
-	public void setWindows(List<Geometry> windows) {
-		this.windows = windows;
-	}
-
-	/**
-		 */
-	protected void clientUpdate(Application application) {
-	}
-
-	/**
-			 */
 	public WindowPlugin(Model model, View view) {
 		super(model);
+		setTags(new String[] { "window" });
 	}
 
 	@Override
+	public void initialize(AppStateManager stateManager, Application app) {
+		super.initialize(stateManager, app);
+		window = (Geometry) app.getAssetManager()
+				.loadModel("Models/window.j3o");
+		initWindows(app);
+	}
+
+	private void initWindows(Application app) {
+		for (Sensor sensor : getSensors()) {
+			boolean open = false;
+			for (Sample sample : sensor.getMesswert()) {
+				if (sample.getTyp().equals("offen")) {
+					open = true;
+					break;
+				}
+			}
+			Material m = new Material(app.getAssetManager(),
+					"Common/MatDefs/Misc/Unshaded.j3md");
+			if (open) {
+				window.open();
+			} else {
+				window.close();
+			}
+			window.setLocalScale(sensor.getPosition().getX(), sensor
+					.getPosition().getY(), sensor.getPosition().getZ());
+			window.setUserData("sid", sensor.getId());
+			windows.add(window.clone());
+		}
+	}
+
 	protected void clientUpdate(Application application, boolean changed) {
-		// TODO Auto-generated method stub
+		if(changed) {
+			updateWindows();
+		}
 
 	}
+	
+	private void updateWindows() {
+		for (Geometry g : windows) {
+			String sid = g.getUserData("sid");
+			for(Sensor sensor: getSensors()) {
+				if(!sensor.getId().equals(sid)) {
+					continue;
+				}
+				for(Sample sample: sensor.getMesswert()) {
+					if(sample.getTyp().equals("zu")) {
+						window.close();
+					} else {
+						window.open();
+					}
+				}
+			}
+		}
+	}
+	
 
 }
