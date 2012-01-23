55da093d5a34c94c70a967e1ea08a1efe2c6c54c
BenediktZ <uhdgk@atudent.kit.edu>
2012-01-23 16:40:52 +0100

    new .xsd / custom static objects and reference points in xml

---
+++ Vision/src/vision/model/StaticGeometry.java
@@ -0,0 +1,120 @@
+package vision.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{}x"/>
+ *         &lt;element ref="{}y"/>
+ *         &lt;element ref="{}angle"/>
+ *         &lt;element ref="{}path"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "x",
+    "y",
+    "angle",
+    "path"
+})
+@XmlRootElement(name = "staticGeometry")
+public class StaticGeometry {
+
+    protected float x;
+    protected float y;
+    protected float angle;
+    @XmlElement(required = true)
+    protected String path;
+
+    /**
+     * Gets the value of the x property.
+     * 
+     */
+    public float getX() {
+        return x;
+    }
+
+    /**
+     * Sets the value of the x property.
+     * 
+     */
+    public void setX(float value) {
+        this.x = value;
+    }
+
+    /**
+     * Gets the value of the y property.
+     * 
+     */
+    public float getY() {
+        return y;
+    }
+
+    /**
+     * Sets the value of the y property.
+     * 
+     */
+    public void setY(float value) {
+        this.y = value;
+    }
+
+    /**
+     * Gets the value of the angle property.
+     * 
+     */
+    public float getAngle() {
+        return angle;
+    }
+
+    /**
+     * Sets the value of the angle property.
+     * 
+     */
+    public void setAngle(float value) {
+        this.angle = value;
+    }
+
+    /**
+     * Gets the value of the path property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * Sets the value of the path property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPath(String value) {
+        this.path = value;
+    }
+
+}
