bdd7b322fbab23fb4019c8e65f20ccb1d9048234
Shinobi18 <thomaskthorn@googlemail.com>
2012-02-02 19:46:46 +0100

    windows at almost correct position

--- Vision/src/vision/view/WindowPlugin.java
+++ Vision/src/vision/view/WindowPlugin.java
@@ -25,6 +25,7 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.SceneGraphVisitor;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.debug.Arrow;
 
 public class WindowPlugin extends Plugin {
 
