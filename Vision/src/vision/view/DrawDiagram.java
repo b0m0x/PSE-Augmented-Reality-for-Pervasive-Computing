61758ff641e766dc69da7f09b802cfa67d206342
zeynep <zeynepguenes@googlemail.com>
2012-02-02 13:29:21 +0100

    ...

--- Vision/src/vision/view/DrawDiagram.java
+++ Vision/src/vision/view/DrawDiagram.java
@@ -3,36 +3,38 @@ package vision.view;
 import java.util.logging.Logger;
 
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.StandardControl;
 import de.lessvoid.nifty.effects.EffectImpl;
 import de.lessvoid.nifty.effects.EffectProperties;
 import de.lessvoid.nifty.effects.Falloff;
 import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.render.NiftyImage;
 import de.lessvoid.nifty.render.NiftyRenderEngine;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.spi.render.RenderImage;
 
 public class DrawDiagram implements EffectImpl {
 
-	private Nifty nifty;
 	Logger log = Logger.getLogger(" ");
 
 	@Override
-	public void activate(Nifty nifty, Element element, EffectProperties parameter) {
-		 		
+	public void activate(Nifty niftyy, Element element, EffectProperties parameter) {
 		
-		
+			
 	}
+	
 
 	@Override
 	public void deactivate() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	private Nifty nifty;
 	@Override
 	public void execute(Element element, float normalizedTime, Falloff falloff,
 			NiftyRenderEngine r) {
-		element.findElementByName("panel-draw-diagram");
-		r.createImage("Texture/walltexture.jpg", true);
-		r.moveTo(300, 100);
-	}
+
+			}
 
 }
