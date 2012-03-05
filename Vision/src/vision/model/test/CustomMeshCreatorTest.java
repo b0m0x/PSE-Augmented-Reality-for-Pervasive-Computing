4f656cdab80abd7318757ed589c296036de002e0
b0m0x <marek.wehmer@googlemail.com>
2012-03-05 21:09:35 +0100

    extend CustomMeshCreator unit tests for increased coverage remove Position -> Sensor assotiation

--- Vision/src/vision/model/test/CustomMeshCreatorTest.java
+++ Vision/src/vision/model/test/CustomMeshCreatorTest.java
@@ -1,6 +1,6 @@
 package vision.model.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,14 +8,15 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.jme3.math.Vector3f;
-import com.jme3.scene.Spatial;
-
 import vision.model.CustomMeshCreator;
 import vision.model.xml.Hole;
 import vision.model.xml.Wall;
 
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
 
 public class CustomMeshCreatorTest {
 
@@ -137,7 +138,7 @@ public class CustomMeshCreatorTest {
 
 		Hole h2 = new Hole();
 		h2.setPositionX1(3f);
-		h2.setPositionY1(1.5f);
+		h2.setPositionY1(0f);
 		h2.setPositionX2(3f);
 		h2.setPositionY2(2f);
 		holes.add(h2);
@@ -149,6 +150,23 @@ public class CustomMeshCreatorTest {
 
 		assertEquals(Math.PI / 2 + Math.PI / 4, ((RigidBodyControl) g.getControl(0)).getPhysicsRotation().toAngleAxis(Vector3f.UNIT_Y), 0.01f);
 		assertEquals(new Vector3f(1.5f, 0f, 0.5f), ((RigidBodyControl) g.getControl(0)).getPhysicsLocation());
+	}
+	
+	@Test
+	public void createCeilingAndFloor() {
+		CustomMeshCreator c = new CustomMeshCreator();
+		Spatial ceiling = c.createCeiling(new Vector3f(1,1,1), new Vector3f(1, 2, 3));
+		assertEquals(new Vector3f(1,1,1), ceiling.getControl(RigidBodyControl.class).getPhysicsLocation());
+		assertEquals(1, ((Box) ((Geometry) ceiling).getMesh()).getXExtent(), 0.0001f);
+		assertEquals(2, ((Box) ((Geometry) ceiling).getMesh()).getYExtent(), 0.0001f);
+		assertEquals(3, ((Box) ((Geometry) ceiling).getMesh()).getZExtent(), 0.0001f);
+		
+		Spatial floor = c.createFloor(new Vector3f(1,1,1), new Vector3f(1, 2, 3));
+		assertEquals(new Vector3f(1,1,1), floor.getControl(RigidBodyControl.class).getPhysicsLocation());
+		assertEquals(1, ((Box) ((Geometry) floor).getMesh()).getXExtent(), 0.0001f);
+		assertEquals(2, ((Box) ((Geometry) floor).getMesh()).getYExtent(), 0.0001f);
+		assertEquals(3, ((Box) ((Geometry) floor).getMesh()).getZExtent(), 0.0001f);
+		
 	}
 	
 
