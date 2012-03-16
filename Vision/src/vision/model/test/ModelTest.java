72fd9d413762faa102c92040ec4b29e8322c927c
BenediktZ <uhdgk@atudent.kit.edu>
2012-03-16 20:33:32 +0100

    z

--- Vision/src/vision/model/test/ModelTest.java
+++ Vision/src/vision/model/test/ModelTest.java
@@ -12,5 +12,22 @@ public class ModelTest {
 	@Test
 	public void test() throws JAXBException {
 		Model model = new Model(null);
+		
+		model.getTaggedSensors(null);
+		model.getSensor();
+		model.getDatenbank();
+		
+		model.setSensor(null);
+		
+		model.getPluginList();
+		model.getPluginControllerList();
+		
+		model.setPluginList(null);
+		model.getGroundplan();
+		
+		model.setGroundplan(null);
+		model.getView();
+		
+		
 	}
 }
